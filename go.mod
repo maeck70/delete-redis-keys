21fa2df5076b2e3a68b082bd6555cbd9dad07425
Marcel van Eck <vaneck.marcel@gmail.com>
2024-10-19 10:08:50 -0700

    delete expiry added

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/maeck70/delete-redis-keys
 
 go 1.22.7
 
-require github.com/gomodule/redigo v1.9.2
+require (
+	github.com/gomodule/redigo v1.9.2
+	github.com/google/uuid v1.6.0
+)
--- main.go
+++ main.go
@@ -1,10 +1,18 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"time"
 
 	"github.com/gomodule/redigo/redis"
+	"github.com/google/uuid"
 )
+
+type Data_t struct {
+	Data       string    `json:"data"`
+	UpdateDTTM time.Time `json:"update-dttm"`
+}
 
 func main() {
 	// Connect to Redis Server
@@ -14,28 +22,113 @@ func main() {
 	}
 	defer conn.Close()
 
+	// createTestKeys(conn)
+	// deleteAllKeys(conn)
+	deleteExpiredKeys(conn, time.Minute)
+	// printAllKeys(conn)
+}
+
+func getAllKeys(conn redis.Conn) map[string]Data_t {
+	dataSet := make(map[string]Data_t)
+
 	// Get all keys
 	keys, err := redis.Strings(conn.Do("KEYS", "*"))
 	if err != nil {
 		panic(err)
 	}
 
-	// Print all keys
+	// Loop through all keys
 	for _, key := range keys {
-		/*
-			// Get the value of the key
-			value, err := redis.String(conn.Do("GET", key))
-			if err != nil {
-				panic(err)
-			}
-			println(value)
-		*/
+		// Get the value of the key
+		value, err := redis.String(conn.Do("GET", key))
+		if err != nil {
+			panic(err)
+		}
 
+		d := Data_t{}
+		err = json.Unmarshal([]byte(value), &d)
+		if err != nil {
+			panic(err)
+		}
+		dataSet[key] = d
+	}
+
+	return dataSet
+}
+
+func deleteAllKeys(conn redis.Conn) {
+	dataSet := getAllKeys(conn)
+
+	// Loop through all keys
+	for key, _ := range dataSet {
 		// Delete the key
-		_, err = conn.Do("DEL", key)
+		_, err := conn.Do("DEL", key)
 		if err != nil {
 			panic(err)
 		}
 		fmt.Printf("Deleted key: %s\n", key)
+	}
+}
+
+func deleteExpiredKeys(conn redis.Conn, expiration time.Duration) {
+	dataSet := getAllKeys(conn)
+
+	cnt := 0
+
+	// Loop through all keys
+	for key, data := range dataSet {
+		checkDttm := time.Now()
+		updateDTTM := data.UpdateDTTM.Add(expiration)
+
+		fmt.Printf("UpdateDTTM: %s\n", updateDTTM)
+		fmt.Printf("CheckDTTM:  %s\n", checkDttm)
+		fmt.Printf("Delete: %v\n", updateDTTM.Before(checkDttm))
+
+		if updateDTTM.Before(checkDttm) {
+			// Delete the key
+			_, err := conn.Do("DEL", key)
+			if err != nil {
+				panic(err)
+			}
+			fmt.Printf("Deleted key: %s\n", key)
+			cnt += 1
+		}
+	}
+	fmt.Printf("Deleted %d keys\n\n", cnt)
+}
+
+func createTestKeys(conn redis.Conn) {
+	for i := 0; i < 10; i++ {
+		// create data
+		data := Data_t{
+			Data:       fmt.Sprintf("value%d", i),
+			UpdateDTTM: time.Now().Add(time.Duration(i*10) * time.Second),
+		}
+
+		jsonData, err := json.Marshal(data)
+		if err != nil {
+			panic(err)
+		}
+
+		key, err := uuid.NewUUID()
+		if err != nil {
+			panic(err)
+		}
+
+		_, err = conn.Do("SET", key.String(), jsonData)
+		if err != nil {
+			panic(err)
+		}
+
+		fmt.Printf("Created key: %s\n\n", key.String())
+	}
+}
+
+func printAllKeys(conn redis.Conn) {
+	dataSet := getAllKeys(conn)
+
+	// Loop through all keys
+	for key, value := range dataSet {
+		fmt.Printf("Key: %s, Value: %s, UpdateDTTM: %s\n", key, value.Data, value.UpdateDTTM)
 	}
 }
